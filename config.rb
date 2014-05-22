ad11e536b685ef6e6e9cf00bdaf102a7023be66f
Jyrki Lilja <jyrki.lilja@focus.fi>
2014-05-22 17:18:34 +0300

    Use autoprefixer

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 require "susy"
-
+require 'autoprefixer-rails'
+require 'csso'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -11,6 +12,7 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
@@ -24,3 +26,14 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+on_stylesheet_saved do |file|
+    css = File.read(file)
+    File.open(file, 'w') do |io|
+        # Produces optimized CSS ready for production
+        #io << Csso.optimize(AutoprefixerRails.process(css).css, true)
+
+        # Produces development friendly CSS
+        io << AutoprefixerRails.process(css)
+    end
+end
